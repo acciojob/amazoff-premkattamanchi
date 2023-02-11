9e9a719da7710cfe35f12abe3c9b8d999303ecc6
prem <premkattamanchi@gmail.com>
2023-02-11 23:08:51 +0530

    coded

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,6 +9,10 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        int hours=Integer.parseInt(deliveryTime.substring(0,2));
+        int mins=Integer.parseInt(deliveryTime.substring(2));
+        this.id=id;
+        this.deliveryTime=hours*60+mins;
     }
 
     public String getId() {
--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -19,10 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("orders")
 public class OrderController {
 
-
+    @Autowired
+    OrderService orderService;
     @PostMapping("/add-order")
     public ResponseEntity<String> addOrder(@RequestBody Order order){
-
+          orderService.addOrder(order);
         return new ResponseEntity<>("New order added successfully", HttpStatus.CREATED);
     }
 
