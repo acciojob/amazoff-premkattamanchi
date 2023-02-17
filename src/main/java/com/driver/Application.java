84a54aa71b2fb5fda70c81cbe12479d52e567a1e
prem <premkattamanchi@gmail.com>
2023-02-17 19:06:52 +0530

    saved 2

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -8,7 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(Application.class, args);
 
 	}
